963cca9da6fd65b3446719634ebbbfcf25bce97a
Jatin Patel <JJPatel@student.fullsail.edu>
2019-07-16 11:00:51 -0400

    added Pickup script and Shop UI script

---
+++ Codebase/1906WorkingTitle/Assets/Scripts/Pickup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private enum Type { Inventory, Coin, Health, Key, EOF};
+
+    [SerializeField] Type type;
+    [SerializeField] Player player;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            switch (type)
+            {
+                case Type.Inventory:
+                    //player.AddToInventory(this);
+                    break;
+                case Type.Coin:
+                    player.AddCoins(5);
+                    break;
+                case Type.Health:
+                    //player.AddHealth(10);
+                    player.TakeDamage();
+                    break;
+                case Type.Key:
+
+                    break;
+                case Type.EOF:
+                    break;
+                default:
+                    break;
+            }
+        }
+        Destroy(gameObject);
+    }
+}
