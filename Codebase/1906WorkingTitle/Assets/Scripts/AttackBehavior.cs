9e16e15111fff8aefa9a7000d62e95755e6730df
Petra Andraschko <PAndraschko@student.fullsail.edu>
2019-07-16 10:15:38 -0400

    Petra sandbox changes.

--- Codebase/1906WorkingTitle/Assets/Scripts/AttackBehavior.cs
+++ Codebase/1906WorkingTitle/Assets/Scripts/AttackBehavior.cs
@@ -15,10 +15,16 @@ public class AttackBehavior : MonoBehaviour
     //If this enemy's attack behavior is enabled or not.
     [SerializeField] bool attackEnabled = true;
 
+    //What projectile the enemy shoots
+    [SerializeField] GameObject projectile;
+
+    private Transform trans;
+
     // Start is called before the first frame update
     void Start()
     {
         attackTimer = Random.Range(attackMin, attackMax);
+        trans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -28,17 +34,22 @@ public class AttackBehavior : MonoBehaviour
         if(attackEnabled && attackTimer <= 0)
         {
             //Chooses and stores a random attack from the available attack list.
-            string temp = attackTypes[Random.Range(0, attackTypes.Count - 1)];
+            string temp = attackTypes[Random.Range(0, attackTypes.Count)];
 
             if(temp.Equals("bullet"))
             {
-
+                Vector3 vect = Vector3.MoveTowards(GameObject.FindGameObjectWithTag("Player").transform.position, GetComponentInParent<Transform>().position, 99);
+                GameObject clone = Instantiate(projectile, transform.position, Quaternion.identity);
+                clone.gameObject.SetActive(true);
+                clone.GetComponent<Rigidbody>().velocity = trans.TransformDirection(vect * 10);
             }
 
             else if(temp.Equals("melee"))
             {
 
             }
+
+            attackTimer = Random.Range(attackMin, attackMax);
         }
     }
 }
