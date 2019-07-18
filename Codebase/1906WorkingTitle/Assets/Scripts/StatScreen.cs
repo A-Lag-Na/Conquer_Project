0565bcb03f6e4badd3b46bf24e0887874497ae8b
Jatin Patel <JJPatel@student.fullsail.edu>
2019-07-18 14:59:35 -0400

    progress on stat screen and its script

--- Codebase/1906WorkingTitle/Assets/Scripts/StatScreen.cs
+++ Codebase/1906WorkingTitle/Assets/Scripts/StatScreen.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatScreen : MonoBehaviour
 {
+    private Button speedBTN, damageBTN, defenseBTN;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //assign buttons
+        speedBTN = transform.Find("Attack Speed").GetChild(0).GetComponent<Button>();
+        damageBTN = transform.Find("Attack Damage").GetChild(0).GetComponent<Button>();
+        defenseBTN = transform.Find("Defense").GetChild(0).GetComponent<Button>();
+
+        //assign funsction listeners
+        speedBTN.onClick.AddListener(AddSpeed);
+        speedBTN.onClick.AddListener(AddDamage);
+        speedBTN.onClick.AddListener(AddDefense);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddSpeed()
     {
-        
+
+    }
+
+    private void AddDamage()
+    {
+
+    }
+
+    private void AddDefense()
+    {
+
     }
 }
