ac51cedfe696721713bdb3f12e3ebfd1772a5b93
Petra Andraschko <PAndraschko@student.fullsail.edu>
2019-08-10 19:19:14 -0400

    Adding more folders for file organization.
    LightningBolt.prefab in progress.

---
+++ Codebase/1906WorkingTitle/Assets/Scripts/BulletSplit.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSplit : MonoBehaviour
+{
+    [SerializeField] private float boltSpeed = 20.0f;
+    [SerializeField] private int numChildren = 0;
+    [SerializeField] GameObject bolts = null;
+
+    private IEnumerable RevertLayer(GameObject hit)
+    {
+        yield return new WaitForSeconds(0.05f);
+        hit.layer = 11;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string tag = collision.collider.tag;
+        if(tag.Equals("Enemy") || tag.Equals("BulletHell Enemy"))
+        {
+
+            List<Vector3> EnemyPositions = new List<Vector3>();
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies)
+            {
+                EnemyPositions.Add(enemy.GetComponent<Transform>().position);
+            }
+
+            if(EnemyPositions.Count > 0)
+            {
+                Collider collider = collision.collider;
+                GameObject hit = collider.gameObject;
+
+                //Sets enemy to a new layer so they do not collide with bolts.
+                hit.layer = 18;
+
+
+                Vector3 currentPosition = hit.GetComponent<Transform>().position;
+
+                EnemyPositions.Remove(currentPosition);
+                
+                for (int i = 0; i < numChildren; i++)
+                {
+                    if (EnemyPositions.Count > 0 && EnemyPositions[i] != null)
+                    {
+                        GameObject bolt = Instantiate(bolts, collider.transform.position, bolts.transform.rotation);
+                        bolt.layer = 12;
+                        bolt.GetComponent<Rigidbody>().velocity = (EnemyPositions[i] - currentPosition) * boltSpeed;
+                        //Zap sound effect could go here
+                    }
+                }
+                StartCoroutine("RevertLayer", hit);
+            }
+        }
+    }
+
+    
+}
