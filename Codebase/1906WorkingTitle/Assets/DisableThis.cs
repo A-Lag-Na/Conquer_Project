8a916338dcbedd5f0510bf98f12db16299239ca9
Petra Andraschko <PAndraschko@student.fullsail.edu>
2019-08-12 16:02:20 -0400

    Cone of Cold.
    Imported ConeCollider asset.

---
+++ Codebase/1906WorkingTitle/Assets/DisableThis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableThis : MonoBehaviour
+{
+    public float _time;
+
+    IEnumerator Timer(float _time)
+    {
+        yield return new WaitForSeconds(_time);
+        gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(Timer(_time));
+    }
+}
+
