1f7798a800c1e391946aa72467630ba59e36e123
Alegna Medina <public.alegna.21@gmail.com>
2019-08-12 16:52:17 -0400

    NavMesh Fixing , Tutorial Level added

---
+++ Codebase/1906WorkingTitle/Assets/Scripts/MountainsTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountainsTrigger : MonoBehaviour
+{
+    GameObject iceBarrier;
+    GameObject fireOne;
+    GameObject fireTwo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iceBarrier = GameObject.Find("Mountain Ice Barrier");
+        fireOne = iceBarrier.transform.GetChild(1).gameObject;
+        fireTwo = iceBarrier.transform.GetChild(2).gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Fire Bullet")
+        {
+            StartCoroutine(BurnPath());
+        }
+
+    }
+
+    IEnumerator BurnPath()
+    {
+        fireOne.SetActive(true);
+        fireTwo.SetActive(true);
+        yield return new WaitForSeconds(2);
+        iceBarrier.SetActive(false);
+        fireOne.SetActive(false);
+        fireTwo.SetActive(false);
+       
+    }
+
+}
---
+++ Codebase/1906WorkingTitle/Assets/Scripts/PlainsTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlainsTrigger : MonoBehaviour
+{
+    GameObject cactusBarrier;
+    GameObject iceEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cactusBarrier = GameObject.Find("Desert Cactus Blockade");
+        iceEffect = cactusBarrier.transform.GetChild(1).gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ice Bullet")
+        {
+            StartCoroutine(FreezePath());
+        }
+
+    }
+
+    IEnumerator FreezePath()
+    {
+        iceEffect.SetActive(true);
+        yield return new WaitForSeconds(2);
+        cactusBarrier.SetActive(false);
+        iceEffect.SetActive(false);
+       
+
+    }
+
+
+}
