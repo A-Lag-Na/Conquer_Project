21d250975437b3baebf3d5b486f9bd2179a1b82b
Taylor Addington <TWAddington@student.fullsail.edu>
2019-08-13 11:37:55 -0400

    FireBall refactor

--- Codebase/1906WorkingTitle/Assets/Scripts/FireBall.cs
+++ Codebase/1906WorkingTitle/Assets/Scripts/FireBall.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
+    #region FireBallProperties
     [SerializeField] GameObject childType = null;
     [SerializeField] float explosionDamage = 2;
+    #endregion
+
     private void OnCollisionEnter(Collision collision)
     {
         if (childType != null)
         {
             GameObject child = Instantiate(childType, transform.position, childType.transform.rotation);
-            child.GetComponent<CreepManager>().SetParticleDamage(explosionDamage/60f);
+            child.GetComponent<CreepManager>().SetParticleDamage(explosionDamage / 60f);
         }
     }
 }
