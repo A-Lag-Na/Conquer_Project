50ab64c65d7cf1542ad7097a93d211385ebcc517
Petra Andraschko <PAndraschko@student.fullsail.edu>
2019-08-13 11:15:14 -0400

    Added check to EnemyAI to prevent errors from using love bullet when only 1 enemy on screen. Please examine EnemyAI comments for details.
    Fixed error caused by setting Color default to null.

--- Codebase/1906WorkingTitle/Assets/Scripts/BulletSplit.cs
+++ Codebase/1906WorkingTitle/Assets/Scripts/BulletSplit.cs
@@ -19,7 +19,6 @@ public class BulletSplit : MonoBehaviour
         string tag = collision.collider.tag;
         if(tag.Equals("Enemy"))
         {
-
             List<Vector3> EnemyPositions = new List<Vector3>();
             GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
             foreach (GameObject enemy in enemies)
