cba1e28766c6827509b70187d42baaba6487778b
Alegna Medina <public.alegna.21@gmail.com>
2019-08-12 23:56:43 -0400

    Small Fixes added Box

---
+++ Codebase/1906WorkingTitle/Assets/Scripts/DropBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBox : MonoBehaviour
+{
+    public GameObject boxPiece;
+    public GameObject spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawner.GetComponent<SpawnScript>().spawnedEnemies.Count == 0)
+        {
+            boxPiece.SetActive(true);
+        }
+    }
+}
