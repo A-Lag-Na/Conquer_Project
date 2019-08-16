cdafe17536abaa29f674035a3723bd07c811a28b
Petra Andraschko <PAndraschko@student.fullsail.edu>
2019-08-16 15:34:59 -0400

    Null check

--- Codebase/1906WorkingTitle/Assets/Scripts/DropBox.cs
+++ Codebase/1906WorkingTitle/Assets/Scripts/DropBox.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class DropBox : MonoBehaviour
 {
-    public GameObject boxPiece = null;
-    private GameObject spawner = null;
+    [SerializeField] GameObject boxPiece = null;
+    [SerializeField] GameObject spawner = null;
     
     void Update()
     {
-        if (boxPiece != null)
+        
+        if (spawner != null && boxPiece != null)
         {
             if (spawner.GetComponent<SpawnScript>().spawnedEnemies.Count > 0)
                 boxPiece.SetActive(false);
