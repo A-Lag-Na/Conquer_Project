22c7b0bf0a970dcd24c172671a88aa95a5c40893
Jatin Patel <JJPatel@student.fullsail.edu>
2019-08-19 11:49:27 -0400

    moved fonts folder, created pickups for all bullets types, created functioning castle door, moved SpikeTrap.cs to scripts folder

--- Codebase/1906WorkingTitle/Assets/Scripts/DropBox.cs
+++ Codebase/1906WorkingTitle/Assets/Scripts/DropBox.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DropBox : MonoBehaviour
 {
-    public GameObject boxPiece = null;
+    public GameObject boxPiece = null, bullet = null;
     public GameObject spawner = null;
     
     void Update()
@@ -13,9 +13,15 @@ public class DropBox : MonoBehaviour
         if (spawner != null && boxPiece != null)
         {
             if (spawner.GetComponent<SpawnScript>().spawnedEnemies.Count > 0)
+            {
                 boxPiece.SetActive(false);
+                bullet.SetActive(false);
+            }
             else
+            {
                 boxPiece.SetActive(true);
+                bullet.SetActive(true);
+            }
         }
     }
 }
---
+++ Codebase/1906WorkingTitle/Assets/Scripts/SpikeTrap.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+
+
+
+    public Animator spikeTrapAnim; //Animator for the SpikeTrap;
+    bool onTrap;
+    GameObject player; //Player Object
+    Player playerScript;//Player Script
+
+    // Use this for initialization
+    void Awake()
+    {
+        //get the Player Component 
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponentInParent<Player>();
+        //get the Animator component from the trap;
+        spikeTrapAnim = GetComponent<Animator>();
+        //start opening and closing the trap for demo purposes;
+        StartCoroutine(OpenCloseTrap());
+
+    }
+
+
+    IEnumerator OpenCloseTrap()
+    {
+        //play open animation;
+        spikeTrapAnim.SetTrigger("open");
+        if (onTrap)
+        {
+            playerScript.TakeDamage(Color.red, 5);
+        }
+        //wait 2 seconds;
+        yield return new WaitForSeconds(2);
+        //play close animation;
+        spikeTrapAnim.SetTrigger("close");
+        //wait 2 seconds;
+        yield return new WaitForSeconds(2);
+        //Do it again;
+        StartCoroutine(OpenCloseTrap());
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            onTrap = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onTrap = false;
+        }
+    }
+
+
+}
