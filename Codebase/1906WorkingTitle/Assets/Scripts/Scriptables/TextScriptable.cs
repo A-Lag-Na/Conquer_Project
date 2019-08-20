dfe6a7debe717431bf08b1a370c2daec7829c352
Alegna Medina <public.alegna.21@gmail.com>
2019-08-20 14:02:40 -0400

    Introduction Loading Screens

---
+++ Codebase/1906WorkingTitle/Assets/Scripts/Scriptables/TextScriptable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Scriptables/String")]
+public class TextScriptable : ScriptableObject
+{
+    [Multiline]
+    public string text;
+}
