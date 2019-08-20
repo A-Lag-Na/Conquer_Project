3a0a5f9f92bc0e806620090fbd70bf53519f5f7d
Petra Andraschko <PAndraschko@student.fullsail.edu>
2019-08-20 12:18:03 -0400

    Code clean

--- Codebase/1906WorkingTitle/Assets/Scripts/DropBox.cs
+++ Codebase/1906WorkingTitle/Assets/Scripts/DropBox.cs
@@ -7,9 +7,6 @@ public class DropBox : MonoBehaviour
     [SerializeField] private GameObject boxPiece = null, bullet = null;
     [SerializeField] private GameObject spawner = null;
     [SerializeField] private GameObject dialogueTrigger;
-    [SerializeField] private GameObject boxPiece = null, bullet = null;
-    [SerializeField] private GameObject spawner = null;
-    [SerializeField] private GameObject dialogueTrigger = null;
  
     void DropLoot()
     {
