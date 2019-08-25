06e61ebd4a4cf4f25f1dc88c7dacdf0997909013
Jatin Patel <JJPatel@student.fullsail.edu>
2019-08-25 18:37:05 -0400

    fixed StatScreen alignment

--- Codebase/1906WorkingTitle/Assets/Scripts/UI/StatScreen.cs
+++ Codebase/1906WorkingTitle/Assets/Scripts/UI/StatScreen.cs
@@ -10,7 +10,7 @@ public class StatScreen : MonoBehaviour
     private float movementSpeed, currentHealth, maxHealth, currentExperience, nextLevelExp, attackSpeed = 0.0f;
     private int defense, damage, level, pointsAvailable = 0;
     private Button speedBTN, damageBTN, defenseBTN = null;
-    private Text levelText, healthText, movementSpeedText, attackSpeedText, damageText, defenseText, pointsText = null;
+    private Text currentLevelText, nextLevelText, healthText, movementSpeedText, attackSpeedText, damageText, defenseText, pointsText = null;
     private RectTransform levelTransform = null;
     private GameObject mainUI = null, attackMax = null;
     private StopWatch stopWatch;
@@ -39,37 +39,20 @@ public class StatScreen : MonoBehaviour
             player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
 
         //assign Texts
-        levelText = transform.Find("Level").GetComponent<Text>();
-        healthText = transform.Find("Health").GetComponent<Text>();
-        movementSpeedText = transform.Find("Speed").GetComponent<Text>();
-        attackSpeedText = transform.Find("Attack Speed").GetComponent<Text>();
-        damageText = transform.Find("Attack Damage").GetComponent<Text>();
-        defenseText = transform.Find("Defense").GetComponent<Text>();
+        currentLevelText = transform.Find("Level").GetChild(1).GetComponent<Text>();
+        nextLevelText = transform.Find("Level").GetChild(2).GetComponent<Text>();
+        healthText = transform.Find("Health").GetChild(0).GetComponent<Text>();
+        movementSpeedText = transform.Find("Speed").GetChild(0).GetComponent<Text>();
+        attackSpeedText = transform.Find("Attack Speed").GetChild(2).GetComponent<Text>();
+        damageText = transform.Find("Attack Damage").GetChild(2).GetComponent<Text>();
+        defenseText = transform.Find("Defense").GetChild(2).GetComponent<Text>();
         pointsText = transform.Find("Available Points").GetComponent<Text>();
         attackMax = transform.Find("Attack Speed").GetChild(1).gameObject;
         attackMax.SetActive(false);
 
         //grab level bar RectTransform
         levelTransform = transform.Find("Level").GetChild(0).GetComponent<RectTransform>();
-
-        //update level
-        levelText.text = $"Level {level}\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{level + 1}";
-
-        //update health
-        healthText.text = $"Current / Max Health\t\t\t\t\t\t\t\t\t{currentHealth} / {maxHealth}";
-
-        //update movement speed
-        movementSpeedText.text = $"Speed\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{movementSpeed}";
-
-        //update attack speed
-        attackSpeedText.text = $"Attack Speed\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{attackSpeed}";
-
-        //update damage
-        damageText.text = $"Attack Damage\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{damage}";
-
-        //update defense
-        defenseText.text = $"Defense\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{defense}";
-
+        
         Time.timeScale = 0;
         Object[] objects = FindObjectsOfType(typeof(GameObject));
         foreach (GameObject go in objects)
@@ -86,7 +69,8 @@ public class StatScreen : MonoBehaviour
         {
             //update level
             level = player.GetLevel();
-            levelText.text = $"Level {level}\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{level + 1}";
+            currentLevelText.text = $"{level}";
+            nextLevelText.text = $"{level + 1}";
 
             //update level bar
             currentExperience = player.GetExperience();
@@ -98,23 +82,23 @@ public class StatScreen : MonoBehaviour
             //update health
             currentHealth = player.GetHealth();
             maxHealth = player.GetMaxHealth();
-            healthText.text = $"Current / Max Health\t\t\t\t\t\t\t\t\t{currentHealth} / {maxHealth}";
+            healthText.text = $"{currentHealth} / {maxHealth}";
 
             //update movement speed
             movementSpeed = player.GetMovementSpeed();
-            movementSpeedText.text = $"Speed\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{(int)movementSpeed}";
+            movementSpeedText.text = $"{(int)movementSpeed}";
 
             //update attack speed
             attackSpeed = player.GetAttackSpeed();
-            attackSpeedText.text = $"Attack Speed\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{attackSpeed}";
+            attackSpeedText.text = $"{attackSpeed}";
 
             //update damage
             damage = player.GetDamage();
-            damageText.text = $"Attack Damage\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{damage}";
+            damageText.text = $"{damage}";
 
             //update defense
             defense = player.GetDefense();
-            defenseText.text = $"Defense\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t{defense}";
+            defenseText.text = $"{defense}";
 
             //update available points
             pointsAvailable = player.GetSpendingPoints();
