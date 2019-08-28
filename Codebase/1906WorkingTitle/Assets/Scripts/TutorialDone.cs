2b18c9cb0eec7a138025987b70db5e11d3ca2eb0
Alegna Medina <public.alegna.21@gmail.com>
2019-08-28 13:41:48 -0400

    Revert "removed old resources folder"
    This reverts commit b0fa35bf372a6970fa9ebb1da739fd3ebe1994be.
     Fixes to Tutorial

--- Codebase/1906WorkingTitle/Assets/Scripts/TutorialDone.cs
+++ Codebase/1906WorkingTitle/Assets/Scripts/TutorialDone.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class TutorialDone : MonoBehaviour
 {
+    [SerializeField] GameObject dialogueCanvas;
+
   //After The Tutorial is done this changes to the game scene.
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            dialogueCanvas.SetActive(false);
             GameObject clone = Instantiate(Resources.Load<GameObject>("Prefabs/UI/SceneLoader"));
             StartCoroutine(clone.GetComponent<SceneLoader>().LoadNewScene(2));
         }
